79ed64eb7649ff885c57aa504afb67e11e653826
Severus7 <joshuasapz@gmail.com>
2023-07-26 17:42:04 +0800

    added country list

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import CityList from "../components/CityList";
+import CountryList from "../components/CountryList";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -44,7 +45,10 @@ const App = () => {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="countries" element={<p>List of Countries</p>} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="form" element={<p>Form</p>} />
         </Route>
       </Routes>
