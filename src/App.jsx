a6dfccaec2ad2f57a96b0b5159ae6fe8fb3abbd5
Severus7 <joshuasapz@gmail.com>
2023-07-26 18:43:46 +0800

    added programmatic navigation

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import CityList from "../components/CityList";
 import CountryList from "../components/CountryList";
+import City from "../components/City";
+import Form from "../components/Form";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -40,16 +42,20 @@ const App = () => {
         <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>List</p>} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
       </Routes>
     </BrowserRouter>
